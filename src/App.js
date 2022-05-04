cc50ea6aa4588109860ee60f95c87fa5737fb2d8
Armuto-Live <luisarmuto3@gmail.com>
2022-05-04 14:09:44 -0500

    propiedades v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import Componente from "./components/Componente";
+import Propiedades from "./components/propiedades";
 function App() {
   let nombre="armuto";
   let auth=true;
@@ -8,22 +9,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{nombre}</h1>
-        <p>
-          {auth ? "el usuario esta logeado":"El usuario no esta logeado"}
-        </p>
-        <p>{2+3}</p>
-        <ul>
-          {estaciones.map((el,index) =>(
-            <li key={index}>{el}</li>
-          ))}
-        </ul>
-        <label htmlFor='nombre'>Nombre</label>
-        <input type="text" id="nombre"/>
-      </header>
-      <section>
-          <Componente msg="Hola soy un componente funcional expresado desde una props"/>
+        <section>
+          <h1>{nombre}</h1>
+          <p>
+            {auth ? "el usuario esta logeado":"El usuario no esta logeado"}
+          </p>
+          <p>{2+3}</p>
+          <ul>
+            {estaciones.map((el,index) =>(
+              <li key={index}>{el}</li>
+            ))}
+          </ul>
+          <label htmlFor='nombre'>Nombre</label>
+          <input type="text" id="nombre"/>
         </section>
+        <section>
+          <Componente msg="Hola soy un componente funcional expresado desde una props"/>
+          <hr/>
+          <Propiedades cadena="Esto es una cadena de texto" numero={19} booleano={false}/>
+        </section>
+      </header>
+          
     </div>
   );
 }
